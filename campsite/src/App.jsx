aa4ea26b8cc7345a83c1016c978ff4b79290e710
Travis <travisjb0@gmail.com>
2023-08-25 17:39:39 -0700

    Quick Refactor

--- campsite/src/App.jsx
+++ campsite/src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Home from './Home'
+import About from './About'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello! Home Page</h1>
+      <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/about' element={<About />} />
+    </Routes>
+    
+  
+  </BrowserRouter>
         
     </>
   )
--- campsite/src/main.jsx
+++ campsite/src/main.jsx
@@ -6,14 +6,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import About from './About.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<App />} />
-    <Route path='/about' element={<About />} />
-    </Routes>
-    
-  
-  </BrowserRouter>
+ <App />
     
  
 )
