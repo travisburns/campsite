ba785bed474b6a0e5b77f8eda7c7fbd02baf6b19
Travis <travisjb0@gmail.com>
2023-08-25 17:36:38 -0700

    Basic routes set up

--- campsite/src/main.jsx
+++ campsite/src/main.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter, routes} from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import About from './About.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <App />
+    <Routes>
+    <Route path='/' element={<App />} />
+    <Route path='/about' element={<About />} />
+    </Routes>
+    
+  
   </BrowserRouter>
     
  
