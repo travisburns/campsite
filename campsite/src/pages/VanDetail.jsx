43b2f0975bd1bf5a322af59d2971fed8b5b4f450
Travis <travisjb0@gmail.com>
2023-08-31 16:42:50 -0700

    Added and understanding useParams with link paths

--- campsite/src/pages/VanDetail.jsx
+++ campsite/src/pages/VanDetail.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
-import {useParams} from "react-router-dom"
-
+import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 
 const VanDetail = () => {
     const params = useParams()
-    console.log(params)
+    const [van, setVan] = React.useState(null)
+
+    React.useEffect(()=> {
+        fetch(`/api/vans/${params.id}`)
+            .then(res => res.json())
+            .then(data => setVan(data.vans))
+    }, [params.id])
+
+
+
   return (
-    <h1>
-      Van detail Page goes here
-    </h1>
+    <div className='van-detail-container'>
+        {van ? (
+            <div className='van-detail'>
+                <img src={van.imageUrl} />
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                <h2>{van.name}</h2>
+                <p className='van-price'><span>${van.price}</span>/day</p>
+                <p>{van.description}</p>
+                <button className='link-button'>Rent this van</button>
+                </div>
+        ) : <h2>Loading...</h2> }
+    </div>
   )
 }
 
