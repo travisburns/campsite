fd4994ca9da145074873c68c418265bb7a1c55b2
Travis <travisjb0@gmail.com>
2023-09-05 15:34:26 -0700

    Added hostvan navbar style links and pages

--- campsite/src/pages/Host/HostVanPhotos.jsx
+++ campsite/src/pages/Host/HostVanPhotos.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HostVanPhotos = () => {
   return (
     <div>
-      ??
+     Photos
     </div>
   )
 }
--- campsite/src/pages/Host/HostVanPricing.jsx
+++ campsite/src/pages/Host/HostVanPricing.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HostVanPricing = () => {
   return (
     <div>
-      ?????
+      Pricing
     </div>
   )
 }
