25ba3e61d1f44634c46552a639caf77770e5e572
Travis <travisjb0@gmail.com>
2023-09-04 17:25:56 -0700

    HostVandetail page completed

--- campsite/src/pages/Host/HostVanDetail.jsx
+++ campsite/src/pages/Host/HostVanDetail.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+
+
 
 const HostVanDetail = () => {
-  return (
+    const {id} = useParams()
+  const [currentVan, setCurrentVan] = React.useState(null)
+    
+
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+        .then(res => res.json())
+        .then(data =>setCurrentVan(data.vans))
+    }, [])
+  
+  if (!currentVan) {
+    return <h1>Loading...</h1>
+  }
+  
+    return (
     <div>
-      Host Van Detail Page
+      <img src={currentVan.imageUrl} />
+      <h2>{currentVan.name}</h2>
+      <p>{currentVan.price}</p>
+      <p>{currentVan.type}</p>
     </div>
   )
 }
