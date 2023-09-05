829a219df9152ae262d1f2b24391a9ce63d21b06
Travis <travisjb0@gmail.com>
2023-09-04 17:06:51 -0700

    Hostvans page with additional routing and fetch request for the data

--- campsite/src/components/HostLayout.jsx
+++ campsite/src/components/HostLayout.jsx
@@ -15,22 +15,32 @@ const HostLayout = () => {
   return (
   <>
       <nav className='host-nav'>
+        
         <NavLink 
         to="/host"
         end={true}
         style={({isActive}) => {isActive ? activeStyles : null}}
         >Dashboard 
         </NavLink>
+
         <NavLink 
         to="/host/income"
           style={({isActive}) => {isActive ? activeStyles : null}}
           >Income 
           </NavLink>
+
         <NavLink 
         to="/host/reviews"
           style={({isActive}) => {isActive ? activeStyles : null}}
         >
           Reviews 
+          </NavLink>
+          
+          <NavLink 
+        to="/host/vans"
+          style={({isActive}) => {isActive ? activeStyles : null}}
+        >
+          Vans
           </NavLink>
       </nav>
       <Outlet />
---
+++ campsite/src/pages/Host/HostVanDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HostVanDetail = () => {
+  return (
+    <div>
+      Host Van Detail Page
+    </div>
+  )
+}
+
+export default HostVanDetail
