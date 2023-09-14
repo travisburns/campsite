cdebb1631f7cf515026980d0f6ca3413c5863a1c
Travis <travisjb0@gmail.com>
2023-09-13 18:14:36 -0700

    Added route loaders

--- campsite/src/App.jsx
+++ campsite/src/App.jsx
@@ -36,21 +36,21 @@ const router = createBrowserRouter(createRoutesFromElements(
     loader ={vansLoader}
     
     />
-  <Route path='/vans/:id' element={<VanDetail />} />
+  <Route path='/vans/:id' element={<VanDetail />} loader={async() => { return null}}   />
 
 
-  <Route path='host' element={<HostLayout />} >
-  <Route index element={<Dashboard />} />
-  <Route path='income' element={<Income />} />
-  <Route path='reviews' element={<Reviews /> } />
-  <Route path='vans' element={<HostVans /> } />
-  <Route path='vans/:id' element={<HostVanDetail /> }>
-  <Route index element={<HostVanInfo /> } />
-  <Route path='pricing' element={<HostVanPricing />} />
-  <Route path='photos' element={<HostVanPhotos />} />
+  <Route path='host' element={<HostLayout />} loader={async() => { return null}} >
+  <Route index element={<Dashboard />} loader={async() => { return null}} />
+  <Route path='income' element={<Income />} loader={async() => { return null}} />
+  <Route path='reviews' element={<Reviews /> } loader={async() => { return null}} />
+  <Route path='vans' element={<HostVans /> } loader={async() => { return null}} />
+  <Route path='vans/:id' element={<HostVanDetail /> } loader={async() => { return null}} >
+  <Route index element={<HostVanInfo /> } loader={async() => { return null}} />
+  <Route path='pricing' element={<HostVanPricing />} loader={async() => { return null}} />
+  <Route path='photos' element={<HostVanPhotos />} loader={async() => { return null}} />
   </Route>
   </Route>
-  <Route path='*' element={<NotFound />}/>
+  <Route path='*' element={<NotFound />} loader={async() => { return null}} />
   </Route>
 
 
